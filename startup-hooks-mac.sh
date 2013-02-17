d8d4f84e28dbce66ec00e665adf4feb235188881
Purinda Gunasekara <purinda@gmail.com>
2013-02-17 20:02:17 +1100

    Modifications to number of scripts

--- startup-hooks-mac.sh
+++ startup-hooks-mac.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 #
-# Place the path to the script in crontab as follows.
-# @reboot /Users/purinda/Projects/dotfiles/startup-hooks-mac.sh
-#
+# Place the path to the script in mac login/logout hooks as follows (Use LogoutHook for logouts)
+# sudo defaults write com.apple.loginwindow LoginHook /path/to/script
 #
 
-DIR=/Volumes/backupdrive
+DIR="/Volumes/backupdrive"
+DOTDIR="/Users/purinda/Projects/dotfiles/"
+
+
+# include helper 
+source $DOTDIR/bash/helper.sh
 
 mount_nas() {
-    mount -t smbfs //guest:@router/backupdrive $DIR
+    mount -t smbfs //guest:@router/backupdrive $DIR >/dev/null 2>&1
+    if [ $? -eq 1 ]
+    then
+        log "NAS not mounted, error occured." "$DOTDIR/logs/boot.log"
+    else
+        log "NAS mounted" "$DOTDIR/logs/boot.log"
+    fi
 }
 
-# Mount NAS
+# if mount directory doesnt exist
 if [ ! -d $DIR ]
 then
     mkdir $DIR
@@ -23,6 +33,6 @@ then
     mount_nas
 fi
 
-
-echo `date "+[INFO] %d/%m/%Y %H:%M:%S"` NAS mounted >> /var/log/custom.log
-
+# Try to fire up the SBS VM
+#VMSTATUS=$("$DOTDIR/safehouse/Control_vm.sh" "start" 2>&1 >/dev/null)
+#log "VM Started: ${VMSTATUS}" "${DOTDIR}/logs/boot.log"
