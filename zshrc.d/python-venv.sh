2d65444a2c57aa5841e2871f707ff829f627710e
Purinda Gunasekara <purinda@gmail.com>
2024-09-03 21:57:00 +1000

    Venv activate shortcut

--- zshrc.d/python-venv.sh
+++ zshrc.d/python-venv.sh
@@ -8,6 +8,20 @@ venv-setup() {
     source "$1/bin/activate"
 }
 
+venv-activate() {
+    if [ -z "$1" ]; then
+        echo "Usage: venv-activate <path>"
+        return 1
+    fi
+
+    if [ ! -d "$1/bin" ]; then
+        echo "No valid virtual environment found at $1"
+        return 1
+    fi
+
+    source "$1/bin/activate"
+}
+
 venv-deactivate() {
     if [ -z "$VIRTUAL_ENV" ]; then
         echo "No virtual environment is activated."
